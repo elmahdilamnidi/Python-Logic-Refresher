9b145900fefcd32a8139b7e1ede80c757c41ca3f
elmahdi lamnidi <261640110+elmahdilamnidi@users.noreply.github.com>
2026-02-19 23:48:31 +0200

    Create sessoin 1.py

---
+++ sessoin 1.py
@@ -0,0 +1,59 @@
+
+"""Q1 :Define a variable programming_language and assign it the value "Python". Then print the 
+variable"""
+
+programming_language = "Python"
+print(programming_language)
+
+"""Q2 :Create a variable num1 and set it to 5. Create another variable num2 and set it to 3. Add 
+these two variables and print the result. """
+
+num1 = 5
+num2 = 3
+result = num1 + num2
+print(result)
+
+"""Q3 :Declare a variable message and assign it the string "Hello, World!". Then, print the message."""
+ 
+message = "Hello, World!"
+print(message)
+
+"""Q4 : Create  a  comment  in  Python  to  explain  the  purpose  of  the  following  line  of  code:  x  = 
+"awesome". Then, print the value of x. """
+
+#the purpose of the following line of code : x="awesome".
+x = "awesome"
+print(x)
+
+"""Q5 :Create  three  variables,  fruit1,  fruit2,  and  fruit3,  and  assign  them  the  values  "apple", 
+"banana",  and  "cherry"  respectively.  Then,  use  the  +  operator  to  concatenate  these  variables  into  a 
+single string and print the result. """
+
+fruit1 = "apple"
+fruit2 = "banana"
+fruit3 = "cherry"
+result = fruit1+fruit2+fruit3
+print(result)
+
+"""Q6 :Create a variable num1 and assign it the value 7. Then, create another variable num2 and 
+assign it the value 3. Write a Python program to subtract num2 from num1 and print the result."""
+
+num1 = 7
+num2 = 3
+result = num1-num2
+print(result)
+
+"""Q7 :Create  a  variable  name  and  assign  it  your  own  name  as  a  string.  Then, use  string 
+concatenation to print a message that says "Hello, [Your Name]!" using the name variable. """
+
+name = "elmahdi"
+message = ("hello," + name + "!")
+print(message)
+
+"""Q8 :Define a variable x and set it to 10. Define another variable y and set it to 3. Calculate and 
+print the result of x divided by y. """
+
+x = 10
+y = 3 
+result = x/y
+print(result)
